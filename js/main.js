3e0d60890a668fb2434f51d3e095ce1e92e23611
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-01-04 14:22:26 +0200

    .

--- js/main.js
+++ js/main.js
@@ -8,9 +8,29 @@ const finalMessage = document.getElementById('final-message');
 
 const figureParts = document.querySelectorAll('figure-part'); 
 
+const words = ['pandemic', 'confirmed case', 'immunity', 'outbreak', 'quarantine', 'self-isolation', 'mask', 'social distancing', 'ventilator', 'antibody', 'isolation'];
 
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-document.addEventListener('DOMContentLoaded', () => {
-	console.log('Hello !');
-	});
+const correctLetters = [];
+const wrongLetters = [];
 
+function displayWord() {
+	wordEl.innerHTML = `
+		${selectedWord
+			.split('')
+			.map(
+				letter => `
+					<span class="letter">
+						${correctLetters.includes(letter) ? letter : ''}
+					</span>
+				`
+			)
+			.join('')
+		}
+	`;
+}
+
+displayWord();
+
+console.log(selectedWord);
