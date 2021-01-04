924a41cad48d27f9c3e64d068f5f37162fa8e628
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-01-04 13:54:18 +0200

    .

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+
+const wordEl = document.getElementById('word');
+const wrongLettersContainerEl = document.getElementById('wrong-letters-container');
+const playAgainBtn = document.getElementById('play-again');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('figure-part'); 
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
 	console.log('Hello !');
 	});
+
